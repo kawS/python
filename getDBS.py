98e1e5bf2250492b0cb0747fa1d66410ccd65385
Kaw$ <16954783@qq.com>
2024-02-23 16:02:36 +0800

    Update getDBS.py

--- getDBS.py
+++ getDBS.py
@@ -7,27 +7,37 @@ import time
 import os
 from opencc import OpenCC
 
-def getList(name):
-  tarUrl = 'https://dbs-cardgame.com/fw/en/cardlist/?search=true&category%5B0%5D=583001'
-  req = requests.get(tarUrl)
-  if req.status_code == 200:
-    soup = BeautifulSoup(req.text, 'html.parser')
-    itemList = soup.find_all('li', class_='cardItem')
-    result = []
-    for item in itemList:
-      src = 'https://dbs-cardgame.com/fw/en/cardlist/' + item.find('a')['data-src']
-      result.append({'src': src})
-    json_ = json.dumps(result, indent = 2, ensure_ascii = False)
-    with open('./dbs/' + name + '.txt', 'w', encoding = 'utf-8') as f:
-      f.write(json_)
+_dict = [
+  { 'series': 'FB01', 'url': 'https://dbs-cardgame.com/fw/en/cardlist/?search=true&category%5B%5D=583001' },
+  { 'series': 'FS01', 'url': 'https://dbs-cardgame.com/fw/en/cardlist/?search=true&q=&category%5B%5D=583101'},
+  { 'series': 'FS02', 'url': 'https://dbs-cardgame.com/fw/en/cardlist/?search=true&q=&category%5B%5D=583102'},
+  { 'series': 'FS03', 'url': 'https://dbs-cardgame.com/fw/en/cardlist/?search=true&q=&category%5B%5D=583103'},
+  { 'series': 'FS04', 'url': 'https://dbs-cardgame.com/fw/en/cardlist/?search=true&q=&category%5B%5D=583104'},
+  { 'series': 'FP', 'url': 'https://dbs-cardgame.com/fw/en/cardlist/?search=true&q=&category%5B%5D=583901'}
+]
 
-def getDet(name, cindex):
-  with open('./dbs/' + name + '.txt', 'r', encoding = 'utf-8') as f:
+def getList():
+  for dictItem in _dict:
+    req = requests.get(dictItem['url'])
+    if req.status_code == 200:
+      soup = BeautifulSoup(req.text, 'html.parser')
+      itemList = soup.find_all('li', class_='cardItem')
+      result = []
+      for item in itemList:
+        src = 'https://dbs-cardgame.com/fw/en/cardlist/' + item.find('a')['data-src']
+        result.append({ 'url': src })
+      json_ = json.dumps(result, indent = 2, ensure_ascii = False)
+      with open('./dbs/' + dictItem['series'] + '.txt', 'w', encoding = 'utf-8') as f:
+        f.write(json_)
+
+def getDet(series, cindex):
+  with open('./dbs/' + series + '.txt', 'r', encoding = 'utf-8') as f:
     strList = f.read()
   rJson = json.loads(strList)
   itemCount = len(rJson)
   index = 1
   result = []
+  path = 'https://dbs-cardgame.com/fw'
   for item in rJson:
     if index <= cindex - 10:
       index += 1
@@ -39,57 +49,66 @@ def getDet(name, cindex):
     s.keep_alive = False
     urllib3.disable_warnings()
     print(str(index) + ' / ' + str(itemCount))
-    req = requests.get(item['src'])
+    req = requests.get(item['url'])
     req.close()
     time.sleep(5)
     if req.status_code == 200:
       cardDet = {}
       soup = BeautifulSoup(req.text, 'html.parser')
+      cardDet['series'] = series
       cardDet['no'] = soup.find('div', class_='cardNo').get_text()
-      nameDom = soup.find_all('h1', class_='cardName')
+      cardDet['rarity'] = soup.find('div', class_='rarity').get_text()
       nameList = []
+      nameDom = soup.find_all('h1', class_='cardName')
       for n in nameDom:
         nameList.append(n.get_text())
       isLeader = True if len(nameList) > 1 else False
+      cardDet['name'] = nameList
       imgList = []
       if isLeader == True:
         imgDom = soup.find_all('div', class_='cardImageImg')
-        for n in imgDom:
-          imgList.append(n.find('img')['src'].replace('../..', 'https://dbs-cardgame.com/fw'))
       else:
-        imgDom = soup.find('div', class_='cardImage')
-        imgList.append(imgDom.find('img')['src'].replace('../..', 'https://dbs-cardgame.com/fw'))
-      cardDet['name'] = nameList
+        imgDom = soup.find_all('div', class_='cardImage')
+      for n in imgDom:
+        imgList.append(n.find('img')['src'].replace('../..', path))
       cardDet['img'] = imgList
       cardData = soup.find_all('div', class_='cardDataRow')
-      d1 = cardData[0].find_all('div', class_='cardDataCell')
-      cardDet['type'] = d1[0].find('div', class_='data').get_text()
-      cardDet['color'] = d1[1].find('div', class_='data').get_text().replace('\n', '')
-      d2 = cardData[1].find_all('div', class_='cardDataCell')
-      cardDet['cost'] = d2[0].find('div', class_='data').get_text()
-      cardDet['spcost'] = d2[1].find('div', class_='data').get_text().replace('\n', '').replace('\r', '').replace(' ', '')
-      d3 = cardData[2].find_all('div', class_='cardDataCell')
-      powerList = []
-      powerDom = d3[0].find_all('div', class_='data')
-      for n in powerDom:
-        powerList.append(n.get_text())
-      cardDet['power'] = powerList
-      cardDet['cbpower'] = d3[1].find('div', class_='data').get_text()
-      cardDet['features'] = cardData[3].find('div', class_='data').get_text().replace('\n', '').replace('\r', '').replace(' ', '')
-      effectList = []
-      effectDom = cardData[4].find_all('div', class_='data')
-      for n in effectDom:
-        temp = n.get_text('|', strip=True).split('|')
-        effectList.append(temp)
-      cardDet['effect'] = effectList
-      cardDet['pack'] = cardData[5].find('div', class_='data').get_text()
+      for i in range(len(cardData)):
+        cell = cardData[i].find_all('div', class_='cardDataCell')
+        if i == 0:
+          cardDet['type'] = cell[0].find('div', class_='data').get_text()
+          cardDet['color'] = cell[1].find('div', class_='colValue').get_text()
+        if i == 1:
+            cardDet['cost'] = cell[0].find('div', class_='data').get_text()
+            cardDet['specifiedCost'] = cell[1].find('div', class_='data').get_text().replace('\n', '').replace('\r', '').replace(' ', '')
+        if i == 2:
+          powerList = []
+          powerDom = cell[0].find_all('div', class_='data')
+          for n in powerDom:
+            powerList.append(n.get_text())
+          cardDet['power'] = powerList
+          cardDet['comboPower'] = cell[1].find('div', class_='data').get_text()
+        if i == 3:
+          cardDet['features'] = cell[0].find('div', class_='data').get_text().replace('\n', '').replace('\r', '').replace(' ', '')
+        if i == 4:
+          effectList = []
+          effectDom = cell[0].find_all('div', class_='data')
+          for n in effectDom:
+            temp = n.get_text('|', strip = True).split('|')
+            effectList.append(temp)
+          cardDet['effect'] = effectList
+        if i == 5:
+          cardDet['productName'] = cell[0].find('div', class_='data').get_text()
       if isLeader == True:
-        cardDet['showF'] = '1'
+        cardDet['isSwitch'] = '1'
+      else:
+        cardDet['isSwitch'] = '-1'
+      cardDet['isShow'] = '1'
       result.append(cardDet)
       index += 1
   s = json.dumps(result, indent = 2, ensure_ascii = False)
-  with open('./dbs/' + name + '-' + str(cindex) + '.json', 'w', encoding = 'utf-8') as f:
+  with open('./dbs/' + series + '-' + str(cindex) + '.json', 'w', encoding = 'utf-8') as f:
     f.write(s)
       
-# getList('FS01')
-getDet('FS01', 10)
+# getList()
+# getDet('FP', 60)
